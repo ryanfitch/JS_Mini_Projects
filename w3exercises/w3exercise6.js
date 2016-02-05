d9b01e0c237245435c227ae17d39e21135564d1f
Ryan Fitch <ryanfitch@mac.com>
2016-02-04 16:31:55 -0800

    Working through the W3 exercises

---
+++ w3exercises/w3exercise6.js
@@ -0,0 +1,5 @@
+// Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.
+
+year = window.prompt("Input a Year : ");  
+x = (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);  
+alert(x);  
