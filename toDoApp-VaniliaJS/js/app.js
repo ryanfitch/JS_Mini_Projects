4d1d6bd26ba2e54e11ae1a42e7aea58a7c8190c6
Ryan Fitch <ryanfitch@mac.com>
2016-03-18 11:34:14 -0700

    working through a todo app

--- toDoApp-VaniliaJS/js/app.js
+++ toDoApp-VaniliaJS/js/app.js
@@ -1,2 +1,29 @@
 //Problem: User interaction doesn't provide desired results.
 //Solution: Add interactivty so the user can manage daily tasks.
+
+var taskInput = document.getElementById("new-task");
+var addButton = document.getElementsByTagName("button")[0];
+var incompleteTasksHolder = document.getElementById("incomplete-tasks");
+var completedTasksHolder= document.getElementById("completed-tasks");
+
+var addTask = function () {
+    console.log("hello")
+}
+
+var editTask = function () {
+    console.log("hello")
+}
+
+var deleteTask = function () {
+    console.log("hello")
+}
+
+var taskCompleted = function () {
+    console.log("hello")
+}
+
+var taskIncomplete = function () {
+    console.log("hello")
+}
+
+addButton.onclick = addTask;
