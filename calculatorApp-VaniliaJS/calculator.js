d61fef0ebcc214ed281caa192482d6de0054a944
Ryan Fitch <ryanfitch@mac.com>
2016-03-22 12:18:51 -0700

    added subtract function

--- calculatorApp-VaniliaJS/calculator.js
+++ calculatorApp-VaniliaJS/calculator.js
@@ -2,11 +2,14 @@ var calculator = {
 		sum: 0,
 		add: function(value) {
                                 this.sum += value;
-    },
-    clear: function() {
-        this.sum = 0;
-    }, 
-    equals: function() {
-        return this.sum;
-    }
+                            },
+                            clear: function() {
+                                this.sum = 0;
+                            }, 
+                            equals: function() {
+                                return this.sum;
+                            },
+                            subtract: function(value) {
+                                this.sum -= value;
+                            }
 }
