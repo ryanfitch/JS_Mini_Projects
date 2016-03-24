83574e71477d1d67c27abb00ae506912a545b34e
Ryan Fitch <ryanfitch@mac.com>
2016-03-24 13:54:51 -0700

    problem sollution planning

--- nodeJS_webProject/app.js
+++ nodeJS_webProject/app.js
@@ -1,15 +1,2 @@
-//Problem:
-//Solution:
-
-//
-
-//
-
-//
-
-//
-
-//
-
-//
-
+//Problem:  We need a simple way to look at a users badge count and javascript points from a web browser.
+//Solution:  Use node.js to perform the profile look ups and serve our templates via HTTP
