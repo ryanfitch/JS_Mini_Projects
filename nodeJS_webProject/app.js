66be5402ec6ac8686695d87638207e6a1594ddf2
Ryan Fitch <ryanfitch@mac.com>
2016-03-24 16:53:41 -0700

    added function homeRoute

--- nodeJS_webProject/app.js
+++ nodeJS_webProject/app.js
@@ -5,10 +5,8 @@
 
 var http = require("http");
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("It's alive!");
-  response.end();
+http.createServer(function (request, response) {
+    homeRoute(request, response);
 }).listen(3000);
 console.log('Server running at 3000');
 
@@ -17,6 +15,16 @@ console.log('Server running at 3000');
         //show search
         //if URL == "/" && "post"
         // redirect to /:username
+function homeRoute(request, response) {
+        if (request.url === "/") {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Header\n");
+        response.write("Search\n");
+        response.end("Footer\n");
+        }
+}
+
+
 
 // 3- Hangle HTTP route Get /:username 
 
