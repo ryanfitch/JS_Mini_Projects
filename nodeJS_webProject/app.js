2d9eb85aff84432dc54c343aa56536a4642ff22f
Ryan Fitch <ryanfitch@mac.com>
2016-03-24 14:00:02 -0700

    problem sollution planning

--- nodeJS_webProject/app.js
+++ nodeJS_webProject/app.js
@@ -1,2 +1,14 @@
 //Problem:  We need a simple way to look at a users badge count and javascript points from a web browser.
 //Solution:  Use node.js to perform the profile look ups and serve our templates via HTTP
+
+// 1- create a web server
+
+// 2- Handle HTTP route Get / Post
+        //if URL === "/" && "get"
+        //show search
+        //if URL == "/" && "post"
+        // redirect to /:username
+
+// 3- Hangle HTTP route Get /:username 
+
+// 4- Function that handles the reading of files and merge in values
