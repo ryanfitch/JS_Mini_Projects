0eaba4654c666e1c543d2c30ca6fb408e952d506
Ryan Fitch <ryanfitch@mac.com>
2016-03-24 14:17:04 -0700

    added server

--- nodeJS_webProject/app.js
+++ nodeJS_webProject/app.js
@@ -3,6 +3,15 @@
 
 // 1- create a web server
 
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    resquest.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:1337/');
+
 // 2- Handle HTTP route Get / Post
         //if URL === "/" && "get"
         //show search
@@ -12,3 +21,4 @@
 // 3- Hangle HTTP route Get /:username 
 
 // 4- Function that handles the reading of files and merge in values
+
