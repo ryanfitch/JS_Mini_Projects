d64512446125e0198f762367eff4f917703e7559
Ryan Fitch <ryanfitch@mac.com>
2016-03-25 12:43:09 -0700

    created routing functionality

--- nodeJS_webProject/router.js
+++ nodeJS_webProject/router.js
@@ -1,3 +1,6 @@
+var Profile = require("./profile.js");
+
+
 function home(request, response) {
         if (request.url === "/") {
         response.writeHead(200, {"Content-Type": "text/plain"});
@@ -7,16 +10,33 @@ function home(request, response) {
         }
 }
 
-// 3- Hangle HTTP route Get /:username 
+// Hangle HTTP route Get /:username 
 
 function user(request, response) {
-    var userName = request.url.replace("/", "")
-    if (userName.length > 0) {
+    var username = request.url.replace("/", "");
+    if (username.length > 0) {
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Header\n");
-        response.write(userName + "\n");
-        response.end("Footer\n");
 
+        // Get json from API
+        var studentProfile = new Profile(username);
+        // On end
+        studentProfile.on("end", function(profileJSON) {
+                // Show profile
+                var values = {
+                    avatarUrl: profileJSON.gravatar_url,
+                    username: profileJSON.profile_name,
+                    badges: profileJSON.badges.length,
+                    javascriptPoints: profileJSON.points.JavaScript
+                };
+                // Simple response
+                response.write(values.username + " has " + values.badges + " badges\n");
+                response.end("Footer\n");
+        });
+        // On 'Error'
+        studentProfile.on("error", function(error){
+                // Show error
+        });
     }
 }
 
