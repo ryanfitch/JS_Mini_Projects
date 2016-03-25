bebaef12c614f43165fbdf29da9c38f337af8cb1
Ryan Fitch <ryanfitch@mac.com>
2016-03-25 16:29:14 -0700

    merged values

--- nodeJS_webProject/renderer.js
+++ nodeJS_webProject/renderer.js
@@ -1,10 +1,17 @@
 var fs = require("fs");
 
+function mergeValues (values, content) {
+    for(var key in values) {
+        content = content.replace("{{" + key + "}}", values[key]);
+    }
+    return content;
+}
+
 function view(templateName, values, response) {
     // read from the template files
-    var fileContents = fs.readFileSync('./views/' + templateName + '.html');
+    var fileContents = fs.readFileSync('./views/' + templateName + '.html', {encoding: "utf8"});
     // insert values in to the content
-
+    fileContents = mergeValues(values, fileContents);
     // write out to the response    
     response.write(fileContents);
 
